649a981630a0a4bf012e2d6cf024b987d5552d79
KLTPL <kltpl@duck.com>
2026-06-23 16:57:25 +0200

    feat: make ghosts slightly slower than pacman

--- src/lib/Pos.ts
+++ src/lib/Pos.ts
@@ -1,3 +1,4 @@
+import { POS_FRACTION_DIGITS } from "./constants";
 import type { Dir } from "./Dir";
 
 export class Pos {
@@ -28,24 +29,18 @@ export class Pos {
 
   isInCenter(): boolean {
     const isXPointFive = (x: number) =>
-      Number((x - 0.5).toFixed(3)) === Math.floor(x);
+      Number((x - 0.5).toFixed(POS_FRACTION_DIGITS)) === Math.floor(x);
     return isXPointFive(this.x) && isXPointFive(this.y);
   }
 
   calcFieldPos(): Pos[] {
     const isXPointFive = (x: number) =>
-      Number((x - 0.5).toFixed(3)) === Math.floor(x); // is like x.5 (x % 0.5 == 0 && !isInteger(x))
+      Number((x - 0.5).toFixed(POS_FRACTION_DIGITS)) === Math.floor(x); // is like x.5 (x % 0.5 == 0 && !isInteger(x))
 
     if (isXPointFive(this.y) && Number.isInteger(this.x)) {
-      return [
-        new Pos(this.x, Math.floor(this.y)),
-        new Pos(this.x - 1, Math.floor(this.y)),
-      ];
+      return [new Pos(this.x, Math.floor(this.y)), new Pos(this.x - 1, Math.floor(this.y))];
     } else if (isXPointFive(this.x) && Number.isInteger(this.y)) {
-      return [
-        new Pos(Math.floor(this.x), this.y),
-        new Pos(Math.floor(this.x), this.y - 1),
-      ];
+      return [new Pos(Math.floor(this.x), this.y), new Pos(Math.floor(this.x), this.y - 1)];
     } else {
       return [new Pos(Math.floor(this.x), Math.floor(this.y))];
     }
@@ -53,8 +48,8 @@ export class Pos {
 
   public static addDirToPos(pos: Pos, dir: Dir, dirMultiplier: number = 1) {
     return new Pos(
-      Number((pos.x + dir.x * dirMultiplier).toFixed(3)),
-      Number((pos.y + dir.y * dirMultiplier).toFixed(3)),
+      Number((pos.x + dir.x * dirMultiplier).toFixed(POS_FRACTION_DIGITS)),
+      Number((pos.y + dir.y * dirMultiplier).toFixed(POS_FRACTION_DIGITS)),
     );
   }
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
+export const POS_FRACTION_DIGITS = 4;
 export const PACMAN_SPEED = 0.1;
-export const GHOST_SPEED = 0.1;
+export const GHOST_SPEED = 0.0625;
